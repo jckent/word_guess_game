8565d922fb01addacb38431c123fef93e8f9c718
jckent <chasekent30@gmail.com>
2019-08-24 12:25:14 -0400

    first fet of js functions added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,8 +6,29 @@ var pokemon = [
     "mew",
 ]
 
+var answerPokemon= "";
+var letters = [];
+var blankLetters = 0;
+var progress = [];
+var wrong = [];
+var wins = 0;
+var losses = 0;
+var remaining = 10;
+
+function guessingGame (){
 var goal = pokemon[Math.floor(Math.random()*pokemon.length
 )];
+
+letters = goal.split("");
+
+blankLetters = letters.length;
+
+for (var i=0; i< blankLetters; i++) {
+    progress.push("_");
+}
+
+document.getElementsByClassName("guessing-field").innerHTML= " " progress.join(" ");
+
 
 var answerPokemon = [];
 for (var i=0; i< pokemon.length; i++) {
@@ -15,3 +36,18 @@ for (var i=0; i< pokemon.length; i++) {
 }
 
 var remainingLetters = word.length;
+
+while (remainingLetters > 0) {
+
+     
+ document.onkeypress = function() {
+ 
+    for (var j= 0; j < word.length; j++) {
+        if (goal[j] === guess) {
+            answerPokemon[j] = guess;
+            remainingLetters--;
+
+        }
+        ]
+    }
+}
